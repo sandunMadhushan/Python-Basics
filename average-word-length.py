e38d3e4bed274c9efb3fe648fceda2df8522ca21
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2023-04-05 04:41:14 +0530

    Create average-word-length.py

---
+++ average-word-length.py
@@ -0,0 +1,20 @@
+# Average Word Length
+
+# Given a sentence as input, calculate and output the average word length of that sentence.
+# To calculate the average word length, you need to divide the sum of all word lengths by the number of words in the sentence.
+
+# Sample Input:
+# this is some text
+
+# Sample Output:
+# 3.5
+
+# Explanation: There are 4 words in the given input, with a total of 14 letters, so the average length will be: 14/4 = 3.5
+# Hint - Strings have a split() method, which returns the string split into a list, with the given separator. By default, the separator is a space, so calling split() will return a list containing the words of the string as items.
+
+sentence = input()
+words = sentence.split()
+num_words = len(words)
+total_length = sum(len(word) for word in words)
+average_length = total_length / num_words
+print(average_length)
