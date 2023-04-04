aa316de2d94ebc8c56dc54c54a750534d1448fb2
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2023-04-05 04:05:58 +0530

    Update list-operations.py

--- list-operations.py
+++ list-operations.py
@@ -1,25 +1,25 @@
-List Operations
+# List Operations
 
-You are given a 2D matrix, which represents the number of people in a room, grouped by their eye color and gender.
-The first row represents the male gender, while the second row represents females.
-The columns are the eye colors, in the following order: brown, blue, green, black
-data = [
-  [23, 11, 5, 14],
-  [8, 32, 20, 5]
-] 
-PY
-The data shows that, for example, there are 20 green eyed females in the room (2nd row, 3rd column).
+# You are given a 2D matrix, which represents the number of people in a room, grouped by their eye color and gender.
+# The first row represents the male gender, while the second row represents females.
+# The columns are the eye colors, in the following order: brown, blue, green, black
+# data = [
+#   [23, 11, 5, 14],
+#   [8, 32, 20, 5]
+# ] 
+# PY
+# The data shows that, for example, there are 20 green eyed females in the room (2nd row, 3rd column).
 
-Our program needs to take eye color as input and output the percentage of people with that eye color in the room.
+# Our program needs to take eye color as input and output the percentage of people with that eye color in the room.
 
-Sample Input:
-blue
+# Sample Input:
+# blue
 
-Sample Output:
-36
+# Sample Output:
+# 36
 
-Explanation: There are 11+32=43 people with blue eyes, which is 36% of the total.
-Note, that the output should be an integer: use the int() function to convert the result.
+# Explanation: There are 11+32=43 people with blue eyes, which is 36% of the total.
+# Note, that the output should be an integer: use the int() function to convert the result.
 
 data = [ 
   [23, 11, 5, 14], 
